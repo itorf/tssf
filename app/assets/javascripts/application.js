7f5c18feaa6f8370abef47096e4b5ad53b7a3102
Ira Torf <i.a.torf@gmail.com>
2014-12-22 16:13:09 -0800

    adjust tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,6 @@
 //= require bootstrap
 //= require tssf
 //= require_tree ../templates
-//= require_tree ./utils
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
