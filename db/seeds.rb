8e7af9c8f15427f375e048ff182d1135caed88dc
Ira Torf <i.a.torf@gmail.com>
2014-12-22 19:43:45 -0800

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+months = (1..12).to_a
+
+days = (1..30).to_a
+
+year = (14..16).to_a
+
+venues = [
+  "The Catalyst",
+  "The Basemnt",
+  "Slims",
+  "Velvet Jones",
+  "Pure"
+]
+
+cities = [
+  "Santa Cruz",
+  "San Francisco",
+  "Santa Barbara",
+  "San Diego",
+  "Los Angeles"
+]
+
+state = "CA"
+tickets = "http://www.iratorf.com/"
+
+10.times do 
+  Show.create(
+  date: "#{months.sample.to_s}/#{days.sample.to_s}/#{year.sample.to_s}",
+  venue: "#{venues.sample}", 
+  city: "#{cities.sample}", 
+  state: "#{state}",
+  tickets_url: "#{tickets}")
+end
